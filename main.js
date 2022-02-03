2e9c78790e8b3559af24d6be7a749f3e74a58132
ImennoArtemiy <90135738+ImennoArtemiy@users.noreply.github.com>
2022-02-03 15:43:48 +0300

    worked with data from the server using the fetch method (#12)

--- main.js
+++ main.js
@@ -134,62 +134,80 @@ plusButtons[2].addEventListener('click', () => {
 
 
 
-const data = [
-  {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-  },
-  {
-    name: 'Apartment Sunshine',
-    city: 'Santa  Cruz de Tenerife',
-    country: 'Spain',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-  },
-  {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-  },
-  {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-  },
-];
+// const data = [
+//   {
+//     name: 'Hotel Leopold',
+//     city: 'Saint Petersburg',
+//     country: 'Russia',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
+//   },
+//   {
+//     name: 'Apartment Sunshine',
+//     city: 'Santa  Cruz de Tenerife',
+//     country: 'Spain',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
+//   },
+//   {
+//     name: 'Villa Kunerad',
+//     city: 'Vysokie Tatry',
+//     country: 'Slowakia',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
+//   },
+//   {
+//     name: 'Hostel Friendship',
+//     city: 'Berlin',
+//     country: 'Germany',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
+//   },
+//   {
+//     name: 'Radisson Blu Hotel',
+//     city: 'Kyiv',
+//     country: 'Ukraine',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
+//   },
+//   {
+//     name: 'Paradise Hotel',
+//     city: 'Guadalupe',
+//     country: 'Mexico',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+//   },
+//   {
+//     name: 'Hotel Grindewald',
+//     city: 'Interlaken',
+//     country: 'Switzerland',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+//   },
+//   {
+//     name: 'The Andaman Resort',
+//     city: 'Port Dickson',
+//     country: 'Malaysia',
+//     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+//   },
+// ];
+//
+// const homesBody = document.getElementById('homes-body');
+//
+// homesBody.innerHTML = data.map(i =>
+//   `<div class="homes__column">
+//     <img src="${i.imageUrl}" alt="House in Russia">
+//     <p class="homes__text -blue-text">${i.name}</p>
+//     <p class="homes__text -gray-text">${i.city}, ${i.country}</p>
+// </div>`).join('');
 
-const homesBody = document.getElementById('homes-body');
+const urlHomesContent = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
 
-homesBody.innerHTML = data.map(i =>
-  `<div class="homes__column">
-    <img src="${i.imageUrl}" alt="House in Russia">
-    <p class="homes__text -blue-text">${i.name}</p>
-    <p class="homes__text -gray-text">${i.city}, ${i.country}</p>
-</div>`).join('');
+fetch(urlHomesContent)
+  .then(data => data.text())
+  .then(data => {
+    return JSON.parse(data);
+  })
+  .then(data => {
+    console.log(data);
+    const homesBody = document.getElementById('homes-body');
+    homesBody.innerHTML = data.map(i =>
+    `<div class="homes__column">
+      <img src="${i.imageUrl}" alt="House in Russia">
+      <p class="homes__text -blue-text">${i.name}</p>
+      <p class="homes__text -gray-text">${i.city}, ${i.country}</p>
+  </div>`).join('');
+  });
